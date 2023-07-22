6e2de4ec3af0b60f9e0e3976d5c0d04f91cba5c5
Cristhian-mb <cristhianc.mosquerab@ecci.educo>
2023-07-21 21:25:43 -0500

    Finalización Punto 3

---
+++ punto-3/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/general-styles.css">
+    <title>Punto 3</title>
+</head>
+<h1>Punto 3</h1>
+<body>
+    <?php echo "Bienvenido al registro de credenciales PHP"?>
+<form method = "GET" action = "ope.php">
+    Nombre <input type = "text" name="n"><br><br>
+    Apellido <input type = "text" name="a"><br><br>
+    Cedula <input type = "text" name="c"><br><br>
+    <input type = "submit" name="Enviar">
+</form>
+</body>
+</html>
---
+++ punto-3/ope.php
@@ -0,0 +1,5 @@
+<?php
+echo "Nombre: ".$_GET['n']."<br>";
+echo "Apellido: ".$_GET['a']."<br>";
+echo "Cédula: ".$_GET['c']."<br>";
+?>
